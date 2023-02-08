5566c33337933ee8e2a6833096079ad962c037d0
mlqpedropb <124839334+mlqpedropb@users.noreply.github.com>
2023-02-08 23:15:36 +0000

    Primeiro codigo git

---
+++ exe01.py
@@ -0,0 +1,11 @@
+n1 = float(input("entre com n1"))
+n2 = float(input("entre com n2"))
+n3 = float(input("entre com n3"))
+me = float(input("entre com me"))
+
+ma = (n1 + n2 * 2 + n3 * 3 + me)/7
+
+if (ma>=9):
+    print("maior ou igual a 9")
+if (ma<4):
+    git addprint("menor que 4"
